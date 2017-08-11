138a02c1c2cfba872c176a2a59b70a33329d47c3
Vasilchikov Aleksey <alekseyvi1303@gmail.com>
2017-08-11 18:25:07 +0300

    стили для страницы new-post.html

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+
+	// new post page: project list
+	$('.js-projectListToggle').click(function(){
+		$('.js-projectList').toggleClass('active');
+
+		return false;
+	});
+	$('.js-projectListAction li a').click(function(){
+		$('.js-projectList').removeClass('active');
+
+		return false;
+	});
 	
 });
