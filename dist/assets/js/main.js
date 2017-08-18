61d45467d7a05fe285850a42c345099d9a29cdd8
vyshalina <vyshalina@gmail.com>
2017-08-18 17:55:06 +0200

    responsive styles

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -6,8 +6,23 @@ $(document).ready(function() {
 
 		return false;
 	});
+	
 	$('.js-projectListAction li a').click(function(){
 		$('.js-projectList').removeClass('active');
+
+		return false;
+	});
+
+	//main-menu
+	$('.my-menu').click(function(){
+		$('.profile-tabs > ul, .my-menu').toggleClass('active');
+
+		return false;
+	});
+
+	//profile tabs
+	$('.responsive-menu').click(function(){
+		$('.main-menu > ul').toggleClass('active');
 
 		return false;
 	});
