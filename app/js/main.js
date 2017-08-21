4d093c2c7b8536b45ae1998083376d0bc6d9d5e1
Вова <vladimir-shalaev22@yandex.ru>
2017-08-21 10:24:23 +0700

    Создаёт каркас для кода приложения, переносит старый код в этот каркас

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,92 @@
-$(document).ready(function() {
+let app = (function ($) {
 
-	// new post page: project list
-	$('.js-projectListToggle').click(function(){
-		$('.js-projectList').toggleClass('active');
+  // ======================================================================== //
+  //                        ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ                           //
+  // ======================================================================== //
 
-		return false;
-	});
-	
-	$('.js-projectListAction li a').click(function(){
-		$('.js-projectList').removeClass('active');
+  /**
+   * Выполняет последовательную инициализацию компонентов приложения
+   * @param components - хэш (объект) с компонентами
+   */
 
-		return false;
-	});
+  function initComponents(components) {
+    Object.getOwnPropertyNames(components).forEach(function (callback) {
+      if (typeof components[callback] !== 'function') {
+        return console.error(`Ошибка! Не возможно инициализировать компонент ${callback} - объект компонента не является функцией`);
+      }
+      components[callback] = components[callback]();
+    });
+  }
 
-	//main-menu
-	$('.my-menu').click(function(){
-		$('.profile-tabs > ul, .my-menu').toggleClass('active');
+  /**
+   * Связывет события уровня приложения с их обработчиками
+   * @param app - экземпляр приложения
+   * @param events - массив дескрипторов событий
+   * @param actions - хэш с обработчиками событий
+   */
 
-		return false;
-	});
+  function bindEvents(app, events, actions) {
+    events.forEach(function (event) {
+      if (event.length === 2) {
+        /* Привязываем событие приложения */
+        $(window).on(event[0], $.proxy(actions[event[1]], app));
+      } else {
+        /* Привязываем событие DOM к приложению */
+        $(event[0]).on(event[1], $.proxy(actions[event[2]], app));
+      }
+    });
+  }
 
-	//profile tabs
-	$('.responsive-menu').click(function(){
-		$('.main-menu > ul').toggleClass('active');
+  // ======================================================================== //
+  //                          ЭКЗЕМПЛЯР ПРИЛОЖЕНИЯ                            //
+  // ======================================================================== //
 
-		return false;
-	});
-	
-});
+  return Object.freeze({
+
+    name: 'One + 1',
+
+    components: {
+      // ==================================================================== //
+      //               ЗДЕСЬ РАЗМЕЩАЕТСЯ КОД КОМПОНЕНТОВ                      //
+      // ==================================================================== //
+
+      /**
+			 * COMPONENT: PROJECT LIST - Выпадающий список проектов
+			 * @return Возвращает содержимое списка в обёртке jQuery
+			 */
+
+      projectList() {
+        let [list, listContent, closeButton] = [
+          '.js-projectListToggle', '.js-projectList',
+          '.js-projectListAction li a'
+        ].map(el => $(el));
+
+        list.click(function toggleList() {
+          listContent.toggleClass('active');
+          return false;
+        });
+
+        closeButton.click(function closeList() {
+          listContent.removeClass('active');
+          return false;
+        });
+
+        return Object.freeze({ listContent });
+      }
+    },
+
+    events: [
+    ],
+
+    actions: {
+    },
+
+    run() {
+      initComponents(this.components);
+      bindEvents(this, this.events, this.actions);
+    }
+  });
+
+})(jQuery);
+
+$(() => { app.run(); });
