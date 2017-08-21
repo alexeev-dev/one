6a476c0a0c3600cd77aeb77c814b7f03ce2e25ee
vyshalina <vyshalina@gmail.com>
2017-08-21 13:04:20 +0200

    last respponsive style

--- app/js/main.js
+++ app/js/main.js
@@ -14,17 +14,34 @@ $(document).ready(function() {
 	});
 
 	//main-menu
+	$('.responsive-menu').click(function(){
+		$('.main-menu > ul').toggleClass('active');
+
+		return false;
+	});
+
+	//things-menu
+	$('.buy-nav-resp').click(function(){
+		$('.buy-nav > ul, .buy-nav-resp').toggleClass('active');
+
+		return false;
+	});
+
+	//filter
+	$('.filter-resp').click(function(){
+		$('.filter-resp, .search-block .search-area, .filter > .select-area, .filter .search-block .price').toggleClass('active');
+
+		return false;
+	});
+
+
+	//profile tabs
 	$('.my-menu').click(function(){
 		$('.profile-tabs > ul, .my-menu').toggleClass('active');
 
 		return false;
 	});
 
-	//profile tabs
-	$('.responsive-menu').click(function(){
-		$('.main-menu > ul').toggleClass('active');
-
-		return false;
-	});
+	
 	
 });
